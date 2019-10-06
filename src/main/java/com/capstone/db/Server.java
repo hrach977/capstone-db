5b1772c5a5f9dad5fbf7aa51be55e739293ac2ee
hrach977 <hrachya.yeghishyan@picsart.com>
2019-10-06 13:33:23 +0400

    changes in server

--- src/main/java/com/capstone/db/Server.java
+++ src/main/java/com/capstone/db/Server.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class Server {
 
-    private CopyOnWriteArrayList<Database> dbs;
+    private CopyOnWriteArrayList<AtomicReference<Database>> dbs;
     private AtomicReference<Database> currentDatabase;
     private String host;
     private String port;
@@ -17,10 +17,13 @@ public class Server {
         this.host = host;
         this.port = port;
 //        currentDatabase = new AtomicReference<>(new Database("test") {});
+        this.dbs = new CopyOnWriteArrayList<>();
         this.currentDatabase = new AtomicReference<>(new Database("test"));
+        dbs.add(currentDatabase);
     }
 
     public void useDb(String dbName) {
-
+        AtomicReference<Database> db = new AtomicReference<>(new Database(dbName));
+        dbs.add(db);
     }
 }
