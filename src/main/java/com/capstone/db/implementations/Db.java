5f741ef1e9ed32be9ca4b2d926d8c1372212f063
hrach977 <hrachya.yeghishyan@picsart.com>
2019-10-07 15:55:32 +0400

    remove redundant package

--- src/main/java/com/capstone/db/implementations/Db.java
+++
@@ -1,6 +0,0 @@
-package com.capstone.db.implementations;
-
-import com.capstone.db.general.Database;
-
-public class Db extends Database {
-}
--- src/main/java/com/capstone/db/indices/Index.java
+++
@@ -1,6 +0,0 @@
-package com.capstone.db.indices;
-
-public abstract class Index {
-
-
-}
